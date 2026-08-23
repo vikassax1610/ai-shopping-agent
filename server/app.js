f378bba4295610e021a1819d42fc3ceaa1bf94ed
vikassax1610 <vikassaxena816@gmail.com>
2026-08-23 20:36:18 +0530

    fixing routing issue

--- server/app.js
+++ server/app.js
@@ -4,9 +4,6 @@ import chatRoute from "./routes/route.chat.js"
 const app = express()
 app.use(cors());
 app.use(express.json())
-app.get("/", (req, res) => {
-  res.send("Hello World")
-})
 app.use("/api", chatRoute)
 
 export default app
