d0e7f4d44d7d3140ee6b51c4b04b0377d69f2b6e
vikassax1610 <vikassaxena816@gmail.com>
2026-08-23 20:28:38 +0530

    ready to deployed

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 import dotenv from "dotenv/config"
 import app from "./app.js"
 const port = process.env.PORT;
+app.get("/", (req, res) => {
+  res.json({
+    message: "AI Shopping Assistant API is running",
+  });
+});
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
