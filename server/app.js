8701deefcd68f3caa052066c3672087b63e39192
vikassax1610 <vikassaxena816@gmail.com>
2026-08-23 20:42:18 +0530

    fixing cors issue

--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ import express from "express"
 import cors from "cors"
 import chatRoute from "./routes/route.chat.js"
 const app = express()
-app.use(cors());
+app.use(cors(
+  {
+    origin: "https://ai-shopping-agent-one.vercel.app/",
+  }
+));
 app.use(express.json())
 app.use("/api", chatRoute)
 
